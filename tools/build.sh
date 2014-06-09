0e98ae609d3a1f02ef7e8a6763c898745838259b
Pellin Broca <brocapellin@outlook.com>
2014-06-09 21:02:11 +0200

    delete all files from public pages

--- tools/build.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -f ".pacmelm-tools-dir" ]; then
-    echo "You must execute this script in tools/";
-    exit;
-fi
-
-cd ../src
-rm -rf ../build/result
-elm --cache-dir=../build/cache --build-dir=../build/result --set-runtime=resources/elm-runtime-0.12.3.js Main.elm
-cp -Rv resources ../build/result
--- tools/release.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -f ".pacmelm-tools-dir" ]; then
-    echo "You must execute this script in tools/";
-    exit;
-fi
-
-cd ..
-cp -Rv build/result pacmelm-$1
-7z a -tzip pacmelm-$1.zip pacmelm-$1
-rm -rf pacmelm-$1
