c2c8446a9e31d2fb807d0a84a92b423bb16aa488
Pellin Broca <brocapellin@outlook.com>
2014-06-14 00:30:44 +0200

    Refactor

--- src/Point.elm
+++ src/Point.elm
@@ -11,9 +11,8 @@ point x y =
     , y = y
     }
 
-distance p1 p2 = sqrt <| (abs (p1.x - p2.x))^2.0 + (abs (p1.y - p2.y))^2.0
+distance p1 p2 =
+    sqrt <|   (abs (p1.x - p2.x))^2.0
+            + (abs (p1.y - p2.y))^2.0
 
-mirror p =
-    { x = p.y
-    , y = p.x
-    }
+mirror p = point p.y p.x
